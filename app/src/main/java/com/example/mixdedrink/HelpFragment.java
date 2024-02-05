a30e34b3881b35b87f8786980db4a5964b838ff5
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-02-06 00:14:44 +0200

    new frag:Help + nav fix

---
+++ app/src/main/java/com/example/mixdedrink/HelpFragment.java
@@ -0,0 +1,37 @@
+package com.example.mixdedrink;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.mixdedrink.databinding.FragmentHelpBinding;
+
+public class HelpFragment extends Fragment {
+    private FragmentHelpBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentHelpBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
