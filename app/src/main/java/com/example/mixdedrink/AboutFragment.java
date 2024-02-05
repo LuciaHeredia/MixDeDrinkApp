eb3109edf01f0e1a1fa6ea2b24544c0cdba6a317
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-02-06 00:07:45 +0200

    new frag:About + nav fix

---
+++ app/src/main/java/com/example/mixdedrink/AboutFragment.java
@@ -0,0 +1,37 @@
+package com.example.mixdedrink;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.mixdedrink.databinding.FragmentAboutBinding;
+
+public class AboutFragment extends Fragment {
+    private FragmentAboutBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentAboutBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
