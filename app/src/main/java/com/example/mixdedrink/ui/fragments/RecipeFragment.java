27f3968aa90fb034513944121c2942c3f26fd1c6
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-02-29 22:05:35 +0200

    floating fav btn

--- app/src/main/java/com/example/mixdedrink/ui/fragments/RecipeFragment.java
+++ app/src/main/java/com/example/mixdedrink/ui/fragments/RecipeFragment.java
@@ -1,43 +1,47 @@
 package com.example.mixdedrink.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import com.example.mixdedrink.databinding.FragmentRecipeBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 public class RecipeFragment extends Fragment {
-
     private FragmentRecipeBinding binding;
+    private Context context;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
 
         binding = FragmentRecipeBinding.inflate(inflater, container, false);
+        listenerSetup();
         return binding.getRoot();
-
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
+    private void listenerSetup() {
         // floating icon setup
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        binding.fab.setOnClickListener(view -> {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            //binding.fab.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.ic_favorites));
         });
-
     }
+
 
     @Override
     public void onStop() {
