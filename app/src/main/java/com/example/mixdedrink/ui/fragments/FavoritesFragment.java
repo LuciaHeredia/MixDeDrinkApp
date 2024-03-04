f7d66d29aae3a412c3bc517e1b513ae8680dc54f
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-03-04 23:18:39 +0200

    tapped FAVORITES icon in Fragment

--- app/src/main/java/com/example/mixdedrink/ui/fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/mixdedrink/ui/fragments/FavoritesFragment.java
@@ -3,11 +3,15 @@ package com.example.mixdedrink.ui.fragments;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
 
 
 import com.example.mixdedrink.R;
@@ -17,31 +21,33 @@ public class FavoritesFragment extends Fragment {
     private FragmentFavoritesBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-
-        setHasOptionsMenu(true);
-    }
-
-    @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
 
         binding = FragmentFavoritesBinding.inflate(inflater, container, false);
+        menuSetup();
         return binding.getRoot();
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    private void menuSetup() {
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu);
 
-    }
+                /* current icon tapped: Favorites */
+                MenuItem item = menu.findItem(R.id.favoritesFragment);
+                if(item!=null)
+                    item.setIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_favorites_tapped, null));
+            }
 
-    @Override
-    public void onPrepareOptionsMenu(Menu menu) {
-        MenuItem item=menu.findItem(R.id.favoritesFragment);
-        if(item!=null)
-            item.setIcon(getResources().getDrawable(R.drawable.ic_favorites_tapped));
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+                // Handle option Menu Here
+            }
+        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
     }
 
     @Override
