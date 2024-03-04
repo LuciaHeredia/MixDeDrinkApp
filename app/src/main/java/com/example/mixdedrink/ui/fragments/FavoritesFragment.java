b38e6930d10f0866a0fffb1123186bee4b405157
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-03-04 23:00:55 +0200

    tapped FAVORITES icon in Fragment

--- app/src/main/java/com/example/mixdedrink/ui/fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/mixdedrink/ui/fragments/FavoritesFragment.java
@@ -2,16 +2,26 @@ package com.example.mixdedrink.ui.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
+import com.example.mixdedrink.R;
 import com.example.mixdedrink.databinding.FragmentFavoritesBinding;
 
 public class FavoritesFragment extends Fragment {
     private FragmentFavoritesBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(
@@ -25,6 +35,13 @@ public class FavoritesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        MenuItem item=menu.findItem(R.id.favoritesFragment);
+        if(item!=null)
+            item.setIcon(getResources().getDrawable(R.drawable.ic_favorites_tapped));
     }
 
     @Override
