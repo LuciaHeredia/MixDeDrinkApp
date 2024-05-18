fc0da0cd7b31e9352bca5cc992ce0e4cc5400380
Lucia Heredia <LuciaHeredia22@gmail.com>
2024-05-18 23:20:19 +0300

    favoriteViewModel

---
+++ app/src/main/java/com/example/mixdedrink/presentation/FavoriteViewModel.java
@@ -0,0 +1,36 @@
+package com.example.mixdedrink.presentation;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.mixdedrink.data.local.repositories.FavoriteRepository;
+import com.example.mixdedrink.data.models.Cocktail;
+
+import java.util.List;
+
+public class FavoriteViewModel extends AndroidViewModel {
+
+    private FavoriteRepository favoriteRepository;
+    private LiveData<List<Cocktail>> allFavorites;
+
+    public FavoriteViewModel(@NonNull Application application) {
+        super(application);
+        favoriteRepository = new FavoriteRepository(application);
+        allFavorites = favoriteRepository.getAllFavorites();
+    }
+
+    public void insertFavorite(Cocktail favorite) {
+        favoriteRepository.insertFavorite(favorite);
+    }
+
+    public void updateFavorite(Cocktail favorite) {
+        favoriteRepository.deleteFavorite(favorite);
+    }
+
+    public LiveData<List<Cocktail>> getAllFavorites() {
+        return allFavorites;
+    }
+}
